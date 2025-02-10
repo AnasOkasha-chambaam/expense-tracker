117d21a0594d387ae5d5e39046951fee3daf73b2
Anas okasha <anasokashachama@gmail.com>
2025-02-10 18:51:10 +0200

    refactor ExpenseList to use Card component for empty state display

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -2,6 +2,7 @@ import { useExpenseStore } from "@/store/expense.store";
 import { InfoIcon } from "lucide-react";
 import React from "react";
 import { ExpenseItem } from "./ExpenseItem";
+import { Card, CardContent } from "./ui/card";
 
 export const ExpenseList: React.FC = () => {
   const { getFilteredExpenses, selectedCategory } = useExpenseStore();
@@ -10,12 +11,16 @@ export const ExpenseList: React.FC = () => {
   return (
     <div className="space-y-4 p-4">
       {filteredExpenses.length === 0 && (
-        <p className="border p-3 bg-muted text-muted-foreground flex items-center gap-2">
-          <InfoIcon />
-          {selectedCategory
-            ? `No expenses found in ${selectedCategory} category`
-            : "No expenses added yet"}
-        </p>
+        <Card>
+          <CardContent className="pt-6">
+            <p className="text-center text-muted-foreground flex items-center justify-center gap-2">
+              <InfoIcon />
+              {selectedCategory
+                ? `No expenses found in ${selectedCategory} category`
+                : "No expenses added yet"}
+            </p>
+          </CardContent>
+        </Card>
       )}
       {filteredExpenses.map((expense) => (
         <ExpenseItem key={expense.id} expense={expense} />
