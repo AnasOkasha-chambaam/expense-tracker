e66bff994f577553241ff213a9c3cc72f3749e51
Anas okasha <anasokashachama@gmail.com>
2025-02-10 16:26:27 +0200

    refactor ExpenseItem component to improve layout and styling; integrate moment-timezone for date formatting and replace delete button with dialog

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,25 +1,28 @@
+import { Expense } from "@/store/expense.store";
+import moment from "moment-timezone";
 import React from "react";
-import { useExpenseStore, Expense } from "@/store/expense.store";
-import { Button } from "@/components/ui/button";
+import DeleteExpenseItemDialog from "./dialogs/DeleteExpenseItem.dialog";
+import { Badge } from "./ui/badge";
 
 interface ExpenseItemProps {
   expense: Expense;
 }
 
 export const ExpenseItem: React.FC<ExpenseItemProps> = ({ expense }) => {
-  const { deleteExpense } = useExpenseStore();
-
   return (
-    <div className="flex justify-between items-center p-4 bg-white shadow rounded-lg">
-      <div>
-        <h3 className="font-semibold">{expense.name}</h3>
-        <p className="text-sm text-gray-500">{expense.category}</p>
-      </div>
-      <div className="flex items-center space-x-4">
-        <span className="font-bold">${expense.amount.toFixed(2)}</span>
-        <Button variant="destructive" onClick={() => deleteExpense(expense.id)}>
-          Delete
-        </Button>
+    <div className="bg-background p-4 shadow rounded-lg">
+      <Badge variant={"secondary"}>{expense.category}</Badge>
+      <div className="flex justify-between items-center">
+        <div>
+          <h3 className="font-semibold mt-1">{expense.name}</h3>
+          <p className="text-sm text-muted-foreground">
+            {moment(expense.createdAt).format("MMMM Do YYYY, h:mm a")}
+          </p>
+        </div>
+        <div className="flex items-center space-x-4">
+          <span className="font-bold">${expense.amount.toFixed(2)}</span>
+          <DeleteExpenseItemDialog expenseId={expense.id} />
+        </div>
       </div>
     </div>
   );
