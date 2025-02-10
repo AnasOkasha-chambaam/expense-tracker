f9f097ba3c2b2ad86bab3f94d2db663ab91137cd
Anas okasha <anasokashachama@gmail.com>
2025-02-10 16:26:30 +0200

    enhance ExpenseList component with empty state message and improved padding; add InfoIcon for better user feedback

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,12 +1,18 @@
-import React from "react";
 import { useExpenseStore } from "@/store/expense.store";
+import { InfoIcon } from "lucide-react";
+import React from "react";
 import { ExpenseItem } from "./ExpenseItem";
 
 export const ExpenseList: React.FC = () => {
   const { expenses } = useExpenseStore();
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 p-4">
+      {expenses.length === 0 && (
+        <p className="border p-3 bg-muted text-muted-foreground flex items-center gap-2">
+          <InfoIcon /> No expenses added yet
+        </p>
+      )}
       {expenses.map((expense) => (
         <ExpenseItem key={expense.id} expense={expense} />
       ))}
